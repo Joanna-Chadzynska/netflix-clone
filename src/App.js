d5e06a3bcabe49fcc587606ce52a611d05938ee6
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-25 19:13:44 +0200

    finish jumbotron

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import jumboData from './fixtures/jumbo.json';
-import { Jumbotron } from './components';
+import { Jumbotron } from './containers';
 
 const App = () => {
 	return (
 		<div className='App'>
-			<Jumbotron.Container>
-				{jumboData.map((jumbo) => (
-					<Jumbotron key={jumbo.id} direction={jumbo.direction}>
-						<Jumbotron.Pane>
-							<Jumbotron.Title>{jumbo.title}</Jumbotron.Title>
-							<Jumbotron.SubTitle>{jumbo.subTitle}</Jumbotron.SubTitle>
-						</Jumbotron.Pane>
-
-						<Jumbotron.Pane>
-							<Jumbotron.Image src={jumbo.image} alt={jumbo.alt} />
-						</Jumbotron.Pane>
-					</Jumbotron>
-				))}
-			</Jumbotron.Container>
+			<Jumbotron />
 		</div>
 	);
 };

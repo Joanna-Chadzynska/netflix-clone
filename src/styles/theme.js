fe48aeca0f2e1032bb5c14f30153bca4113f759c
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-25 20:02:33 +0200

    setup theme

---
+++ src/styles/theme.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+	colors: {
+		gray: {
+			veryLight: '#e5e5e5',
+			veryLight2: '#e6e6e6',
+			light: '#757575',
+			light3: '#737373',
+			light4: ' #8c8c8c',
+			middle: '#303030',
+			dark: '#333333',
+		},
+		black: '#000000',
+		white: '#ffffff',
+		green: {
+			primary: '#008000',
+		},
+		red: {
+			primary: '#FF0000',
+			secondary: '#e50914',
+			btn: '#ff0a16',
+			btnLink: '#f40612',
+			btnPlay: '#ff1e1e',
+		},
+		orange: '#e87c03',
+	},
+};
+
+const Theme = ({ children }) => (
+	<ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+export default Theme;

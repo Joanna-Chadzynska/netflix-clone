c8c9b41d05440409cce13a5bbb9dff01a68a5559
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-25 15:29:33 +0200

    add jumbotron

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import jumboData from './fixtures/jumbo.json';
+import { Jumbotron } from './components';
 
 const App = () => {
 	return (
 		<div className='App'>
-			<header className='App-header'>Learn React</header>
+			<Jumbotron.Container>
+				{jumboData.map((jumbo) => (
+					<Jumbotron key={jumbo.id} direction={jumbo.direction}>
+						<Jumbotron.Pane>
+							<Jumbotron.Title>{jumbo.title}</Jumbotron.Title>
+							<Jumbotron.SubTitle>{jumbo.subTitle}</Jumbotron.SubTitle>
+						</Jumbotron.Pane>
+
+						<Jumbotron.Pane>
+							<Jumbotron.Image src={jumbo.image} alt={jumbo.alt} />
+						</Jumbotron.Pane>
+					</Jumbotron>
+				))}
+			</Jumbotron.Container>
 		</div>
 	);
 };

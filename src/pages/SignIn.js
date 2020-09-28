8d3aa790b3e03fc8e80af534a7fa2b3419cb421d
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-28 21:16:14 +0200

    update sign in page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
+import { Footer, HeaderWithLogo } from '../containers';
 
 const SignIn = () => {
-	return <div>sign in</div>;
+	return (
+		<>
+			<HeaderWithLogo>
+				<Form>
+					<Form.Title>Sign In</Form.Title>
+					<Form.Base noValidate>
+						{/* <Form.Label htmlFor='email'>Email or phone number</Form.Label> */}
+						<Form.Input
+							id='email'
+							type='email'
+							name='email'
+							placeholder='Email or phone number'
+						/>
+						{/* <Form.Label htmlFor='password'>Password</Form.Label> */}
+						<Form.Input
+							id='password'
+							type='password'
+							name='password'
+							placeholder='Password'
+						/>
+						<Form.Submit>Sign In</Form.Submit>
+						<Form.Help>
+							<div>
+								<Form.Input type='checkbox' name='checkbox' id='checkbox' />
+								<Form.Label htmlFor='checkbox'>
+									<span>Remember me</span>
+								</Form.Label>
+							</div>
+
+							<Form.Link to={ROUTES.LOGIN_HELP}>Need help?</Form.Link>
+						</Form.Help>
+					</Form.Base>
+
+					<Form.Text>Login with Facebook</Form.Text>
+
+					<Form.Text>
+						New to Netflix?{' '}
+						<Form.Link to={ROUTES.SIGN_UP}>Sign up now</Form.Link>
+					</Form.Text>
+
+					<Form.TextSmall>
+						This page is protected by Google reCAPTCHA to ensure you're not a
+						bot.
+						<a href='!#'>Learn more.</a>
+					</Form.TextSmall>
+				</Form>
+			</HeaderWithLogo>
+
+			<Footer />
+		</>
+	);
 };
 
 export default SignIn;

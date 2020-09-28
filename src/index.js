8f2c1f96affd6a80aba323d134d8e63aea7c7f7a
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-28 18:19:23 +0200

    create firebase context

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import 'normalize.css';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { FirebaseContext } from './context/firebase';
+import { firebase } from './lib/firebase.prod';
 import { GlobalStyles } from './styles/globalStyles';
 import Theme from './styles/theme';
 
 render(
 	<React.StrictMode>
-		<Theme>
-			<GlobalStyles />
-			<App />
-		</Theme>
+		<FirebaseContext.Provider value={{ firebase }}>
+			<Theme>
+				<GlobalStyles />
+				<App />
+			</Theme>
+		</FirebaseContext.Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );

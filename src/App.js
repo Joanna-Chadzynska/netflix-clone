77afda1779826809d6b13628854f7883c7b9624f
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-29 17:47:25 +0200

    change routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 import { Browse, Home, LoginHelp, NotFound, SignIn, SignUp } from './pages';
 
 const App = () => {
+	const { user } = useAuthListener();
+
 	return (
 		<Router>
 			<Switch>
-				<Route exact path={ROUTES.HOME}>
-					<Home />
-				</Route>
-				<Route exact path={ROUTES.SIGN_IN}>
+				<IsUserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.SIGN_IN}>
 					<SignIn />
-				</Route>
-				<Route exact path={ROUTES.SIGN_UP}>
+				</IsUserRedirect>
+				<IsUserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.SIGN_UP}>
 					<SignUp />
-				</Route>
-				<Route exact path={ROUTES.BROWSE}>
+				</IsUserRedirect>
+				<ProtectedRoute user={user} path={ROUTES.BROWSE}>
 					<Browse />
-				</Route>
-				<Route exact path={ROUTES.LOGIN_HELP}>
+				</ProtectedRoute>
+				<IsUserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.HOME}>
+					<Home />
+				</IsUserRedirect>
+				<IsUserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.LOGIN_HELP}>
 					<LoginHelp />
-				</Route>
+				</IsUserRedirect>
 				<Route path='*'>
 					<NotFound />
 				</Route>

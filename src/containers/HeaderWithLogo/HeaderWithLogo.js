b78e37a9ca68d068faaa07b415d3e7438b58b368
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-29 19:54:31 +0200

    add bg boolean value

--- src/containers/HeaderWithLogo/HeaderWithLogo.js
+++ src/containers/HeaderWithLogo/HeaderWithLogo.js
@@ -3,9 +3,9 @@ import { Header } from '../../components';
 import * as ROUTES from '../../constants/routes';
 import logo from '../../logo.svg';
 
-const HeaderWithLogo = ({ children }) => {
+const HeaderWithLogo = ({ children, bg = true }) => {
 	return (
-		<Header>
+		<Header bg={bg}>
 			<Header.Frame>
 				<Header.Logo src={logo} to={ROUTES.HOME} alt='Netflix' />
 			</Header.Frame>

bf14473c198cbb088e9b0cf0458d5dd2d80e2c4d
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-30 17:44:19 +0200

    create loading component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { Link as ReachRouterLink } from 'react-router-dom';
-import { Background, ButtonLink, Container, Logo } from './styles/header';
+import {
+	Background,
+	ButtonLink,
+	Container,
+	Dropdown,
+	Feature,
+	FeatureCallOut,
+	Group,
+	Link,
+	Logo,
+	Picture,
+	PlayButton,
+	Profile,
+	Text,
+} from './styles/header';
 
 const Header = ({ bg = true, children, ...restProps }) => {
 	return bg ? <Background {...restProps}>{children}</Background> : children;
 };
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+	return <Feature {...restProps}>{children}</Feature>;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
 	return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+	return <Group {...restProps}>{children}</Group>;
 };
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
@@ -20,6 +42,37 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
 	return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+	return <PlayButton {...restProps}>{children}</PlayButton>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+	return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+	return <Link {...restProps}>{children}</Link>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+	children,
+	...restProps
+}) {
+	return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+	return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+	return <Picture src={`/images/users/${src}.png`} {...restProps} />;
+};
+
+Header.Dropdown = function HeaderDropdown({ src, ...restProps }) {
+	return <Dropdown src={`/images/users/${src}.png`} {...restProps} />;
 };
 
 export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ export { default as Footer } from './Footer';
 export { default as Form } from './Form';
 export { default as Header } from './Header';
 export { default as Jumbotron } from './Jumbotron';
-// export * from './Loading';
+export { default as Loading } from './Loading';
 export { default as OptForm } from './OptForm';
 // export * from './Player';
 export { default as Profiles } from './Profiles';

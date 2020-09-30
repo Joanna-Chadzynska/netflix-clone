c152e8f01e295fe8222f3982fc1cf44141c8d120
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-30 17:44:40 +0200

    add handle profile method

--- src/containers/Profiles/Profile.js
+++ src/containers/Profiles/Profile.js
@@ -3,13 +3,18 @@ import { Profiles } from '../../components';
 import { HeaderWithLogo } from '../index';
 
 const SelectProfile = ({ user, setProfile }) => {
+	const handleProfile = () =>
+		setProfile({
+			displayName: user.displayName,
+			photoURL: user.photoURL,
+		});
 	return (
 		<>
 			<HeaderWithLogo bg={false} />
 			<Profiles>
 				<Profiles.Title>Who's watching?</Profiles.Title>
 				<Profiles.List>
-					<Profiles.User>
+					<Profiles.User onClick={handleProfile}>
 						<Profiles.Picture src={user.photoURL} />
 						<Profiles.UserName>{user.displayName}</Profiles.UserName>
 					</Profiles.User>
